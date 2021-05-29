fce748955373fe64587a8b5208a7d3469649ebd3
ya1k96 <yamil123_1@live.com>
2021-05-29 20:33:38 -0300

    update

--- public/scripts/my-script.js
+++ public/scripts/my-script.js
@@ -4,7 +4,7 @@ $(function() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("/sw.js")
+        .register("../sw.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
