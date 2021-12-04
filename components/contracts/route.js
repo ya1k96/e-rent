f4d842ccba50096aa88c7ea5a11c3b687f7a4f67
ya1k96 <yamil123_1@live.com>
2021-12-04 12:47:37 -0300

    FIX-01: Agragada la ruta y el controlador para listar contratos

--- components/contracts/route.js
+++ components/contracts/route.js
@@ -5,6 +5,7 @@ const {notEmptyId} = require('../../middlewares/forms');
 const router = require('express').Router();
 
 router.post('/', validContract, create);
+router.get('/', notEmptyId, getAll);
 router.get('/:id', notEmptyId, getById);
 router.put('/:id', update);
 router.delete('/:id', remove);
